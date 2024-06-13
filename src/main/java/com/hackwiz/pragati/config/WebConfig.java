d7e9ce52342dcfa2b02829168c922e1c5a5f7271
vibhore2773 <vibhore2773@gmail.com>
2024-06-14 05:01:40 +0530

    config update

--- src/main/java/com/hackwiz/pragati/config/WebConfig.java
+++ src/main/java/com/hackwiz/pragati/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
