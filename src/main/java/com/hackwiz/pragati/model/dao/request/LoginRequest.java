d66c4d3c51570f24aae2a7e1e595fefb394074b4
vibhore2773 <vibhore2773@gmail.com>
2024-06-13 20:21:08 +0530

    junk

---
+++ src/main/java/com/hackwiz/pragati/model/dao/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.hackwiz.pragati.model.dao.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginRequest {
+    private String mobileNumber;
+    private String password;
+}
