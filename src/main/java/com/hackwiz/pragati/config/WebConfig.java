3e3730e4f1510d75ac1a6fcd7023c01b3139cd82
vibhore2773 <vibhore2773@gmail.com>
2024-06-14 05:06:48 +0530

    config update

--- src/main/java/com/hackwiz/pragati/config/WebConfig.java
+++ src/main/java/com/hackwiz/pragati/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.hackwiz.pragati.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/v1/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
