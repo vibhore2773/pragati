424a737df9d5bbe4c35dffb0f22ae9f205f8bb70
Sparshal Kothari <sparshal.kothari@meesho.com>
2024-06-13 20:45:49 +0530

    register login API

--- src/main/java/com/hackwiz/pragati/config/RedisConfig.java
+++ src/main/java/com/hackwiz/pragati/config/RedisConfig.java
@@ -3,10 +3,16 @@ package com.hackwiz.pragati.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.time.Duration;
 
 @Configuration
 @EnableRedisRepositories
@@ -14,8 +20,10 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(getRedisConfiguration());
+        connectionFactory.afterPropertiesSet();
+        return connectionFactory;
     }
 
     @Bean
@@ -24,5 +32,13 @@ public class RedisConfig {
         template.setConnectionFactory(redisConnectionFactory());
         log.info("RedisTemplate initialized {}, ", template);
         return template;
+    }
+
+
+
+    private RedisConfiguration getRedisConfiguration() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("redis-10017.c31597.asia-seast1-mz.gcp.cloud.rlrcp.com", 10017);
+        config.setPassword("jsrxakedjeyfdddvdv");
+        return config;
     }
 }
