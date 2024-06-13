94fcc17755a4080e214177b1a799fda9c1080dbb
vibhore2773 <vibhore2773@gmail.com>
2024-06-14 01:15:11 +0530

    static Skill Fetch API

---
+++ src/main/java/com/hackwiz/pragati/models/responses/GetStaticSkillResponse.java
@@ -0,0 +1,20 @@
+package com.hackwiz.pragati.models.responses;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class GetStaticSkillResponse {
+    List<Skill> skills;
+
+    @Data
+    @Builder
+    public static class Skill {
+        private String name;
+        private String imageUrl;
+        private float trendingRate;
+    }
+}
